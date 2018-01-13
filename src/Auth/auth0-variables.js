711e9c953040695d2715e38877dc66dee18445e2
bestegriffin <griffinbeste@gmail.com>
2018-01-12 22:45:07 -0500

    working app

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'coinchat.auth0.com',
+    clientId: '4fwRsscl8sNuG0P30sS3qvN2tGzadVyw',
+    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+}
